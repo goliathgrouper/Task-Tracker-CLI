c491ff1688ce742bdfb2865091f1a141a82caf74
goliathgrouper <vitalijsimon48@gmail.com>
2024-11-03 13:32:00 +0300

    Handle mark-in-progress

--- tasker.py
+++ tasker.py
@@ -1,4 +1,4 @@
-#! ..\venv\Scripts\python.exe
+#! ..\venv\Scripts\python.exe 
 import sys, json, datetime
 from tabulate import tabulate
 
@@ -17,25 +17,38 @@ def main():
     elif argc > 3:
         sys.exit("Too many arguments provided")
 
+    # Add handler
     if arguments[0] == "add" and argc == 2:
         add_task(choose_id(), arguments[1])
         if argc != 2:
             sys.exit("Incorrect usage!")
 
-    if arguments[0] == "list" and argc == 1:
-        list_tasks()
-    # Wait for input before closeing
+    if arguments[0] == "list":
+        if argc == 1:
+            list_tasks()
+
+    if arguments[0] == 'mark-in-progress':
+        if argc != 2:
+            sys.exit('Incorrect usage!')
+        mark_in_progress(arguments[1])
+        
+            
+        
+        
+
+
+    # Wait for input before closing
     input()
 
 
 # Add todo task
-def add_task(id: int, description: str):
+def add_task(aid: int, description: str):
     with open(TASKLIST, "r") as readfile:
         data = json.load(readfile)
     now = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     data.append(
         {
-            "id": id,
+            "id": aid,
             "description": description,
             "status": "todo",
             "created_at": now,
@@ -44,7 +57,7 @@ def add_task(id: int, description: str):
     )
     with open(TASKLIST, "w") as writefile:
         json.dump(data, writefile)
-    print(f"Task added successfully (ID: {id})")
+    print(f"Task added successfully (ID: {aid})")
 
 
 def list_tasks():
@@ -72,9 +85,9 @@ def choose_id():
     for task in data:
         used_ids.append(task["id"])
 
-    for id in range(1, 101):
-        if id not in used_ids:
-            return id
+    for aid in range(1, 101):
+        if aid not in used_ids:
+            return aid
 
 
 def ensure_tasklist():
@@ -84,6 +97,34 @@ def ensure_tasklist():
         tasklist.close()
     except FileExistsError:
         pass
+
+# Returns True if id is used else False
+def check_for_id(aid): # its 'aid' not 'id' because of std function id in python
+    with open(TASKLIST, 'r') as rf:
+        data = json.load(rf)
+    used_ids = []
+    for task in data:
+        used_ids.append(task["id"])
+    return aid in used_ids
+
+def mark_in_progress(aid):
+    try:
+        aid = int(aid)
+        if aid <= 0:
+            sys.exit('Id should be a positive integer')
+    except ValueError:
+        sys.exit('Id should be a positive integer')
+    if not(check_for_id(aid)):
+        sys.exit('No task with this id')
+    with open(TASKLIST, 'r') as rf:
+        data = json.load(rf)
+    for i in range(len(data)):
+        if data[i]['id'] == aid:
+            data[i]['status'] = 'in-progress'
+    with open(TASKLIST, 'w') as wf:
+        json.dump(data, wf)
+        
+
 
 
 if __name__ == "__main__":
